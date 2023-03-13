2986769639bb96b4408b0284b1f8bc9fb8ee5ba3
MickeyTheDrone <rgbasrawi@rageedbasrawi.tech>
2023-03-12 18:42:06 -0700

    Assignment done!

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+let messages = [];
+
+function writeCards(name, event){
+    for (let i = 0; i < name.length; i ++){
+       let message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+        
+    }
+    return messages;
+debugger;
+}
+
+function countDown(number){
+
+while (number >= 0){
+    console.log (number);
+    number--;
+}
+
+}
